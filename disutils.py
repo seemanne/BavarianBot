01c0782bc63198925920b37c09f967dfa22e451f
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2022-11-22 19:00:17 +0100

    Add Snipeboard (#3)
    * add snipeboard
    
    * add disutils
    
    * remove random non-response
    
    * remove selfsnail

---
+++ disutils.py
@@ -0,0 +1,46 @@
+import discord
+import pandas as pd
+import random
+
+async def display_table(message: discord.Message, table: pd.DataFrame, title = 'Unnamed embed'):
+    content = ''
+    printlist = table.values.tolist()
+    for entry in printlist:
+        content += str(entry) + '\n'
+    embed = discord.Embed(title = title, description= content )
+    await message.channel.send(embed = embed)
+    
+async def messageCarousel(message: discord.Message, responses: list):
+    rand = random.random()
+    rand = rand * (len(responses) - 1)
+    rand = round(rand)
+    await message.channel.send(responses[rand])
+
+async def dynamic_display_table(message: discord.Message, table: pd.DataFrame, title = 'Unnamed embed', **kwargs):
+    """
+    pass on a dict titled custom_format and print message with key starting with § indicating a table key and the others coming in plain text
+    """
+    content = ''
+    printlist = table.values.tolist()
+    if ('custom_format' in kwargs.keys()):
+        for entry in printlist:
+            for key, value in kwargs.custom_format:
+                if key.startswith('§'):
+                    content += entry[value] + ' '
+                else: 
+                    content += value
+            content += '\n'
+    else:
+        for entry in printlist:
+            content += str(entry) + '\n'
+    embed = discord.Embed(title = title, description = content)
+    try: 
+        await message.channel.send(embed = embed)
+    except Exception as e:
+        await message.channel.send('Embed creation failed with error: ' + e)
+    
+def numeral_adverb(occurence_count: int):
+    if occurence_count == 1: return 'once'
+    if occurence_count == 2: return 'twice'
+    return f'{occurence_count} times'
+
