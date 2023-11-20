60d4f7b9580aa8a2f7c59af20589c3995803c77a
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-11-20 22:35:23 +0100

    add run scripts

---
+++ scripts/init.sh
@@ -0,0 +1,22 @@
+set -e
+podman build -f ./CONTAINERFILE -t bavarianbot:latest .
+nohup podman-compose -f podman-compose.yml up &
+
+echo "giving the container time to start"
+sleep 5
+
+curl -X 'GET' \
+  'http://localhost:8000/service/init_db' \
+  -H 'accept: application/json'
+
+curl -X 'PUT' \
+  'http://localhost:8000/service/start' \
+  -H 'accept: application/json'
+
+curl -X 'PUT' \
+  'http://localhost:8000/client/activate' \
+  -H 'accept: application/json'
+
+curl -X 'GET' \
+  'http://localhost:8000/service/status' \
+  -H 'accept: application/json'
