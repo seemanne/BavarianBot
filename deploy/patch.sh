b502f25a09c0a624d2933d87754af315fc1801d2
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-11-25 18:02:38 +0100

    expose current patch

--- deploy/patch.sh
+++ deploy/patch.sh
@@ -2,6 +2,8 @@ set -e
 
 current_date_time=$(date "+%Y_%m_%d_%H_%M")
 
+git rev-parse --short HEAD >> ./current_patch
+
 curl -X 'GET' \
   'http://localhost:8000/poast?channel=446853953905229844&message=Brb%2C%20getting%20an%20upgrade' \
   -H 'accept: application/json'
