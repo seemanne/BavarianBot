3ca9693d708e255e23cd587644f290f58ff95241
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2024-11-14 21:21:32 +0100

    start putting text on needle

--- src/images/needle.py
+++ src/images/needle.py
@@ -1,7 +1,7 @@
 import math
 import io
 import logging
-from PIL import Image
+from PIL import Image, ImageDraw
 
 LOG = logging.getLogger("uvicorn")
 
@@ -21,8 +21,17 @@ CACHED_NEEDLE = CACHED_NEEDLE.resize(
 LOG.info(f"background image loaded, size: {CACHED_BACKGROUND.size}")
 
 
+def get_text(abs_score):
+    if abs_score <= 2:
+        return "Lean "
+    if abs_score <= 5:
+        return "Likely "
+    return "Very Likely"
+
 def get_needle_into_buffer(net_score):
     buffer = io.BytesIO()
+    abs_score = abs(net_score)
+    scaling_factor = 0.0 + abs_score * 0.3 if abs_score < 4 else 1
     im = Image.alpha_composite(
         CACHED_BACKGROUND,
         CACHED_NEEDLE.rotate(
@@ -30,6 +39,7 @@ def get_needle_into_buffer(net_score):
             center=(1500 // RESIZE_FACTOR, 1465 // RESIZE_FACTOR),
         ),
     )
+    
     im.save(buffer, "png")
     buffer.seek(0)
     LOG.debug(f"Saved image to buffer, size: {im.size}")
